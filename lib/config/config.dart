4cd2dd420988614280ce86a2aa52f143c49db1a1
Dan <64694785+daniel-price@users.noreply.github.com>
2020-07-02 16:49:49 +0100

    Added config file to gitignore

--- lib/config/config.dart
+++
@@ -1,7 +0,0 @@
-class Configuration {
-  static const String IDENTIFIER = "YOUR-IDENTIFIER";
-  static const String SECRET = "YOUR-SECRET";
-  static const String CREATE_POST_URL = "https://auth.truelayer.com/connect/token";
-  static const String CALLBACK_URL_SCHEME = "bankr"; //Also need to set this in AndroidManifest.xml
-  static const String CALLBACK_URL_HOST = "auth"; //Also need to set this in AndroidManifest.xml
-}
